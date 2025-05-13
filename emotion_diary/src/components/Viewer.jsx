c2d4649b87d086a9ee7891d663219333b8de2b40
김수현 <hvl1789@gepplus.co.kr>
2025-05-13 17:43:22 +0900

    diary 작업 중

--- emotion_diary/src/components/Viewer.jsx
+++ emotion_diary/src/components/Viewer.jsx
@@ -1,13 +1,18 @@
+import { getEmotionImage } from "../util/emotion_image";
 const Viewer = () => {
+  const emotionId = 2;
+
   return(
     <div className="box__viewer">
       <p className="text">오늘의 감정</p>
-      <div>
-        <img src="" alt="" />
+      <div className="box__emotion-area">
+        <img src={getEmotionImage(emotionId).src} alt="" />
         <p>그럭저럭</p>
       </div>
-      <p className="text">오늘의 일기</p>
-      <textarea className="form__textarea" />
+      <div className="box__text-area">
+        <p className="text">오늘의 일기</p>
+        <textarea className="form__textarea" />
+      </div>
     </div>
   )
 }
---
+++ emotion_diary/src/util/constants.js
@@ -0,0 +1,22 @@
+export const emotionList = [
+  {
+    emotionId:emotion1,
+    emotionName:'완전 좋음'
+  },
+  {
+    emotionId:emotion2,
+    emotionName:'좋음'
+  },
+  {
+    emotionId:emotion3,
+    emotionName:'보통'
+  },
+  {
+    emotionId:emotion4,
+    emotionName:'나쁨'
+  },
+  {
+    emotionId:emotion5,
+    emotionName:'끔찍함'
+  },
+]
